c48cc89e6879f32ea1fa4f5f398bb3d373817b96
Miki Nomura <miki@fyi.tokyo>
2016-07-22 11:00:19 +0900

    updated unity ver

---
+++ Assets/Resources/Script/ChangeAllChildValue.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+
+namespace acFonts{
+
+	[RequireComponent(typeof(AudioSource))]
+	public class ChangeAllChildValue : MonoBehaviour {
+
+		private Renderer[] renderers;
+
+		private float value1;
+		private float value2;
+		private float value3;
+		private float value4;
+		private float value5;
+		private float value6;
+		private float value7;
+		public Color color;
+		private GameObject Value1SliderGet;
+		private GameObject Value2SliderGet;
+		private GameObject Value3SliderGet;
+		private GameObject Value4SliderGet;
+		private GameObject Value5SliderGet;
+		private GameObject Value6SliderGet;
+		private GameObject Value7SliderGet;
+		private Color randomColorX;
+		private Color randomColorY;
+
+		public Slider value1Slider;
+		public Slider value2Slider;
+		public Slider value3Slider;
+		public Slider value4Slider;
+		public Slider value5Slider;
+		public Slider value6Slider;
+		public Slider value7Slider;
+		private Slider valueSlider;
+		public Button colorButton;
+		public Button meshBottun;
+
+		public bool soundEffect;
+
+		public bool colorChange;
+
+		//for sound
+		private AudioSource audio_;
+		float[] waveData_ = new float[1024];
+
+		//for sound FFT
+		public int resolution = 1024;
+		public Transform lowMeter, midMeter, highMeter;
+		public float lowFreqThreshold = 14700, midFreqThreshold = 29400, highFreqThreshold = 44100;
+		public float lowEnhance = 1f, midEnhance = 10f, highEnhance = 100f;
+
+		// Use this for initialization
+		void Start () {
+
+
+			renderers = GetComponentsInChildren<Renderer>();
+
+			//set custom shader
+			Shader mainShader = Resources.Load ("acFonts/Vertex Animation") as Shader;
+			for (int i = 0; i < renderers.Length; i++) {
+				//renderers [i].material.shader = mainShader;
+			}
+
+			//Adds a listener to the main slider and invokes a method when the value changes.
+			ChangeColor();
+
+			value1Slider.onValueChanged.AddListener (delegate {ChangeParameter (1);});
+			value2Slider.onValueChanged.AddListener (delegate {ChangeParameter (2);});
+			value3Slider.onValueChanged.AddListener (delegate {ChangeParameter (3);});
+			value4Slider.onValueChanged.AddListener (delegate {ChangeParameter (4);});
+			value5Slider.onValueChanged.AddListener (delegate {ChangeParameter (5);});
+			value6Slider.onValueChanged.AddListener (delegate {ChangeParameter (6);});
+			value7Slider.onValueChanged.AddListener (delegate {ChangeParameter (7);});
+
+
+			colorButton.onClick.AddListener(delegate {ChangeColor ();});
+
+			audio_ = GetComponent<AudioSource>();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			switch(soundEffect){
+			case true:
+				ChangeParameterWithSound ();
+				break;
+			case false:
+				//ChangeParameter ();
+				break;
+			default:
+				return;
+ 			}
+
+		}
+
+		public void ChangeParameter(int number){
+
+			var name = "Value " + number + " Slider";
+			var propertyName = "_Value" + number;
+
+			Value1SliderGet = GameObject.Find (name);
+			value1 = Value1SliderGet.GetComponent<Slider> ().value;
+
+			for (int i = 0; i < renderers.Length; i++) {
+				if (number == 6) {
+					renderers[i].transform.eulerAngles = new Vector3(90,180 - value1,0);
+				} else {
+					renderers [i].material.SetFloat (propertyName, Random.Range (0, value1));
+				}
+
+			}
+			ChooseLetter ();
+		}
+
+		public void ChangeColor(){
+			randomColorX = new Color(Random.value, Random.value, Random.value, 1.0F);
+			randomColorY = new Color(Random.value, Random.value, Random.value, 1.0F);
+
+			for (int i = 0; i < renderers.Length; i++) {
+				renderers [i].material.SetColor ("_ColorX", randomColorX);
+				renderers [i].material.SetColor ("_ColorY", randomColorY);
+			}
+
+			if (colorChange) {
+				//set color to button
+				var imageMaterial = colorButton.GetComponent<Image> ().material;
+				imageMaterial.SetColor ("_Color", randomColorX);
+				imageMaterial.SetColor ("_Color2", randomColorY);
+			}
+
+		}
+
+		void ChangeTransform(float vol){
+			for (int i = 0; i < renderers.Length; i++) {
+				renderers [i].transform.localScale = new Vector3(500,500,500) + Vector3.one * vol * 200;
+			}
+		}
+
+		void ChangeParameterWithSound(){
+
+			AudioListener.GetOutputData(waveData_, 1);
+			var volume = waveData_.Select(x => x*x).Sum() / waveData_.Length;
+			//ChangeParameterWithSound (volume);
+
+			//for sound FFT
+			var spectrum = AudioListener.GetSpectrumData(resolution, 0, FFTWindow.BlackmanHarris);
+			var deltaFreq = AudioSettings.outputSampleRate / resolution;
+			float low = 0f, mid = 0f, high = 0f;
+
+			for (var i = 0; i < resolution; ++i) {
+				var freq = deltaFreq * i;
+				if      (freq <= lowFreqThreshold)  low  += spectrum[i];
+				else if (freq <= midFreqThreshold)  mid  += spectrum[i];
+				else if (freq <= highFreqThreshold) high += spectrum[i];
+			}
+
+			low  *= lowEnhance;
+			mid  *= midEnhance;
+			high *= highEnhance;
+
+			value4 = low * 0.05F;
+			value3 = high * 2F;
+			for (int i = 0; i < renderers.Length; i++) {
+				renderers [i].material.SetFloat ("_Value3", value3);
+				renderers [i].material.SetFloat ("_Value4", value4);
+
+			}
+		}
+
+		void ChooseLetter(){
+			for (int i = 0; i < renderers.Length; i++) {
+				var abc = "s";
+				if (abc.Contains (renderers [i].name)) {
+					print ("contain:" + renderers [i].name);
+				}
+			}
+		}
+
+			
+	}
+}
---
+++ Assets/Resources/Script/MeshOnOff.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace acFonts{
+	public class MeshOnOff : MonoBehaviour {
+
+		public GameObject MeshGroup;
+		public GameObject FontsGroup;
+
+		private bool sound;
+
+		public void Start(){
+			MeshGroup.SetActive (false);
+			MeshGroup.GetComponent<ChangeAllChildValue> ().colorChange = false;
+			FontsGroup.SetActive (true);
+			FontsGroup.GetComponent<ChangeAllChildValue> ().colorChange = true;
+			sound = false;
+		}
+
+		public void onClick(){
+
+			if (MeshGroup.activeSelf) {
+				MeshGroup.SetActive (false);
+				MeshGroup.GetComponent<ChangeAllChildValue> ().colorChange = false;
+			} else {
+				MeshGroup.SetActive (true);
+				MeshGroup.GetComponent<ChangeAllChildValue> ().colorChange = true;
+				MeshGroup.GetComponent<ChangeAllChildValue> ().ChangeColor ();
+			}
+
+			if (FontsGroup.activeSelf) {
+				FontsGroup.SetActive (false);
+				FontsGroup.GetComponent<ChangeAllChildValue> ().colorChange = false;
+			} else {
+				FontsGroup.SetActive (true);
+				FontsGroup.GetComponent<ChangeAllChildValue> ().colorChange = true;
+				FontsGroup.GetComponent<ChangeAllChildValue> ().ChangeColor ();
+			}
+				
+		}
+
+		public void onClickSound(){
+			ChangeAllChildValue target = FontsGroup.GetComponent<ChangeAllChildValue> ();
+			ChangeAllChildValue meshTarget = MeshGroup.GetComponent<ChangeAllChildValue> ();
+
+			if (sound) {
+				meshTarget.soundEffect = false;
+				target.soundEffect = false;
+				sound = false;
+				meshTarget.ChangeParameter (3);
+				meshTarget.ChangeParameter (4);
+				GameObject.Find ("Value 3 Slider").GetComponent<Slider> ().interactable = true;
+				GameObject.Find ("Value 4 Slider").GetComponent<Slider> ().interactable = true;
+				target.ChangeParameter (4);
+				target.ChangeParameter (4);
+			} else {
+				meshTarget.soundEffect = true;
+				target.soundEffect = true;
+				sound = true;
+				GameObject.Find ("Value 3 Slider").GetComponent<Slider> ().interactable = false;
+				GameObject.Find ("Value 4 Slider").GetComponent<Slider> ().interactable = false;
+			}
+		}
+	}
+}
