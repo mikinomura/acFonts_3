fab12ba34705389319ba10f3dc6949ba9646a4be
Miki Nomura <miki@fyi.tokyo>
2016-07-22 12:08:09 +0900

    add midiJack plugin

---
+++ Assets/KnobIndicator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using MidiJack;
+
+public class KnobIndicator : MonoBehaviour
+{
+	public int knobNumber;
+	public int parameterNumber;
+	float maxValue;
+	float sliderValue;
+
+	void Start()
+	{
+		sliderValue = gameObject.GetComponent<Slider>().value;
+		maxValue = gameObject.GetComponent<Slider> ().maxValue;
+	}
+
+	void Update()
+	{
+		var channels = MidiMaster.GetKnobNumbers();
+		var s = MidiMaster.GetKnob(knobNumber);
+		knobNumber = channels [parameterNumber];
+		print (s);
+		print (sliderValue);
+		gameObject.GetComponent<Slider>().value = s * maxValue;
+	}
+}
